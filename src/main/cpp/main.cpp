9a06d746c6bb2738ac648f01775406bf39727104
tony.astolfi <tony.astolfi@tamr.com>
2016-11-07 13:28:37 -0500

    add more diagnostics when hostname resolution fails

--- src/main/cpp/main.cpp
+++ src/main/cpp/main.cpp
@@ -10,14 +10,22 @@ int main(int argc, char **argv) {
   boost::asio::ip::tcp::resolver resolver(io);
   boost::asio::ip::tcp::endpoint ep[2];
   for (int i = 0; i < 2; ++i) {
-    boost::asio::ip::tcp::resolver::query q(argv[1 + i * 2], argv[2 + i * 2]);
-    auto it = resolver.resolve(q);
-    decltype(it) end;
-    if (it == end) {
-      std::cerr << "failed to resolve bind endpoint" << std::endl;
-      return 1;
+    std::string host_name = argv[1 + i * 2];
+    std::string service_port = argv[2 + i * 2];
+    boost::asio::ip::tcp::resolver::query q(host_name, service_port);
+    try {
+      auto it = resolver.resolve(q);
+      decltype(it) end;
+      if (it == end) {
+        std::cerr << "failed to resolve bind endpoint" << std::endl;
+        return 1;
+      }
+      ep[i] = *it;
+    } catch (...) {
+      std::cerr << "resolving " << host_name << ":" << service_port
+                << std::endl;
+      throw;
     }
-    ep[i] = *it;
   }
 
   ProxyServer server(io, ep[0], ep[1]);
